8185fe5b894383d130579c08835fcdd4329c5508
Ricardo Pacheco <rjgpacheco@gmail.com>
2021-03-28 16:52:35 +0100

    Added more JupyterLab packages

--- my-jupyter/post_conda_install.sh
+++ my-jupyter/post_conda_install.sh
@@ -29,6 +29,12 @@ jupyter server extension enable --user jupyterlab_code_formatter
 mkdir /Users/ricardo/Library/Caches/black/20.8b1/
 
 
+# Themes
+ jupyter labextension install @arbennett/base16-nord
+ jupyter labextension install @arbennett/base16-one-dark
+ jupyter labextension install @arbennett/base16-mexico-light
+ jupyter labextension install @arbennett/base16-summerfruit-light
+
 
 # Check if everything is OK.
 jupyter labextension list
